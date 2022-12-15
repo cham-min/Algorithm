321e8a268bec040efa50e825565ecedfb1be60b4
hyunmin Choi <96946274+cham-min@users.noreply.github.com>
2022-12-15 23:15:31 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
@@ -0,0 +1,11 @@
+function solution(num_list) {
+    let odd = 0;
+    let even = 0;
+    
+    for (let i = 0; i < num_list.length; i++) {
+        if (num_list[i] % 2) even++;
+        else odd++;
+    }
+    
+    return [odd, even];
+}
