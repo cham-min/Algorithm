fc5de3bafc19884a4c85f0b117179691c3b8f534
hyunmin Choi <96946274+cham-min@users.noreply.github.com>
2023-01-07 18:55:00 +0900

    [level 0] Title: 문자열안에 문자열, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120908. 문자열안에 문자열/문자열안에 문자열.js
+++ 프로그래머스/lv0/120908. 문자열안에 문자열/문자열안에 문자열.js
@@ -1,5 +1,6 @@
 function solution(str1, str2) {
-    if (str1.indexOf(str2) >= 0) return 1;
+    // if (str1.indexOf(str2) >= 0) return 1;
+    // return 2;
     
-    return 2;
+    return str1.split(str2).length > 1 ? 1 : 2
 }
