8dfd7cff9b5203e7c583174f082167e5d9ce6aac
hyunmin Choi <96946274+cham-min@users.noreply.github.com>
2023-01-13 23:42:05 +0900

    [level 0] Title: 점의 위치 구하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120841. 점의 위치 구하기/점의 위치 구하기.js
+++ 프로그래머스/lv0/120841. 점의 위치 구하기/점의 위치 구하기.js
@@ -2,5 +2,10 @@ function solution(dot) {
     const [x, y] = dot;
     const check = x * y > 0;
     
+    // if (x > 0 && y > 0) return 1;
+    // if (x < 0 && y > 0) return 2;
+    // if (x < 0 && y < 0) return 3;
+    // if (x > 0 && y < 0) return 4;
+    
     return x > 0 ? (check ? 1 : 4) : (check ? 3 : 2);
 }
